44e7d338d2a78556a8d8ef335ab7027b968369ea
Fady <fady.fouad.a@gmail.com>
2019-04-03 15:29:03 +0200

    update note database

--- app/src/main/java/com/etatech/architecturecomponents/NoteDatabase.java
+++ app/src/main/java/com/etatech/architecturecomponents/NoteDatabase.java
@@ -1,5 +1,6 @@
 package com.etatech.architecturecomponents;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.db.SupportSQLiteOpenHelper;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.DatabaseConfiguration;
@@ -7,6 +8,7 @@ import android.arch.persistence.room.InvalidationTracker;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
 @Database(entities = {Note.class}, version = 1)
@@ -21,9 +23,35 @@ public abstract class NoteDatabase extends RoomDatabase {
             instances = Room.databaseBuilder(context.getApplicationContext(),
             NoteDatabase.class,"note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallBack)
                     .build();
         }
         return instances;
+    }
+
+    private static RoomDatabase.Callback roomCallBack = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDBAsyncTask(instances).execute();
+        }
+    };
+
+    private static class PopulateDBAsyncTask extends AsyncTask<Void ,Void ,Void>{
+
+        private NoteDao noteDao ;
+
+        private PopulateDBAsyncTask(NoteDatabase database){
+            noteDao = database.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("title 1","Desc 1",1));
+            noteDao.insert(new Note("title 2","Desc 2",2));
+            noteDao.insert(new Note("title 3","Desc 3",3));
+            return null;
+        }
     }
 
 }
